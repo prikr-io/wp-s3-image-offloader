8278bd8e14411809b95564e1759f4989b1bc7801
Koen Dolron <koen@prikr.io>
2023-06-27 11:18:45 +0200

    old updater file

--- prikr-image-offloader.php
+++ prikr-image-offloader.php
@@ -3,7 +3,7 @@
 Plugin Name: Prikr Image offloader
 Plugin URI: https://prikr.io
 Description: Offload WP images to S3.
-Version: 1.3
+Version: 1.0
 Author: Prikr
 Author URI: https://prikr.io/
 License: GPLv2 or later
