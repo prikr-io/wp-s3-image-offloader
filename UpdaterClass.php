7a9257ec8e55a27209f087e22622c6cfd6428f67
Koen Dolron <koen@prikr.io>
2023-06-27 10:04:44 +0200

    updater

--- UpdaterClass.php
+++ UpdaterClass.php
@@ -3,7 +3,7 @@
 defined('ABSPATH') || exit;
 
 if (!class_exists('prikrUpdateChecker')) {
-
+    var_dump(plugin_basename(__DIR__) . '.php');
     class prikrUpdateChecker
     {
 
@@ -15,13 +15,11 @@ if (!class_exists('prikrUpdateChecker')) {
 
         public function __construct()
         {
-            // var_dump('construct');
+            var_dump('UpdaterClass');
             $this->plugin_slug = plugin_basename(__DIR__);
             $this->plugin_basefile = plugin_basename(__DIR__) . '.php';
-            var_dump($this->plugin_slug);
-            var_dump($this->plugin_basefile);
             $this->version = '1.0';
-            $this->cache_key = 'prikr_updater_cache_3';
+            $this->cache_key = 'prikr_updater_cache_1';
             $this->cache_allowed = false;
             delete_transient($this->cache_key);
 
