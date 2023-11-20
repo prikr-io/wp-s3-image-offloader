4e6e8839ba5a2016f50b22539524bb4a63cfad25
Koen Dolron <koen@prikr.io>
2023-11-20 13:41:43 +0100

    Updated versioning of plugin

--- updater/update-version.js
+++ updater/update-version.js
@@ -1,104 +1,126 @@
-/**
- * For this plugin to update, we'd need to update the version in 2 seperate places. 
- * In both the info.json file and the main plugin file (prikr-image-offloader.php).
- * For semantics, we'll also update the package.json file.
- */
-const fs = require('fs');
+const fs = require('fs').promises;
 const path = require('path');
+const readline = require('readline');
 
 const infoJsonFile = path.join(__dirname, '..', 'info.json');
 const mainPluginFile = path.join(__dirname, '..', 'prikr-image-offloader.php');
 const packageJsonFile = path.join(__dirname, '..', 'package.json');
 
-// Read and increment the current version by 0.1
-function incrementVersion(version) {
-    const [major, minor] = version.split('.').map(Number);
-    return `${major}.${(minor + 1)}`;
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+let newVersion; // Variable to store the user-inputted version
+
+// Function to get the current version from a file
+async function getCurrentVersionFromFile(filePath) {
+    try {
+        const fileContent = await fs.readFile(filePath, 'utf8');
+        const versionMatch = /"version": "(\d+\.\d+\.\d+)"/.exec(fileContent);
+        return versionMatch ? versionMatch[1] : null;
+    } catch (err) {
+        console.error(`Error reading version from ${filePath}:`, err);
+        return null;
+    }
 }
 
-// Update the JSON file
-function updateInfoJsonFile() {
-    fs.readFile(infoJsonFile, 'utf8', (err, data) => {
-        if (err) {
-        console.error('Error reading the JSON file:', err);
-        return;
-        }
+// Function to prompt the user for the new version
+async function promptForNewVersion() {
+    const currentVersion = await getCurrentVersionFromFile(packageJsonFile) || '0.0.1';
 
-        try {
-        const json = JSON.parse(data);
-        const currentVersion = json.version;
-        const newVersion = incrementVersion(currentVersion);
-        json.version = newVersion;
-
-        fs.writeFile(infoJsonFile, JSON.stringify(json, null, 2), 'utf8', (err) => {
-            if (err) {
-            console.error('Error writing the updated JSON file:', err);
-            } else {
-            console.log(`Version updated to ${newVersion} in JSON file`);
-            }
+    async function ask() {
+        return new Promise((resolve) => {
+            rl.question(`Enter new version (current version ${currentVersion}): `, (version) => {
+                resolve(version);
+            });
         });
-        } catch (parseError) {
-        console.error('Error parsing JSON:', parseError);
-        }
-    });
-}
+    }
 
-// Update the PHP file
-function updateMainPluginFile() {
-    fs.readFile(mainPluginFile, 'utf8', (err, data) => {
-        if (err) {
-        console.error('Error reading the PHP file:', err);
-        return;
-        }
+    let isValid = false;
 
-        // Use regular expression to find and replace the version attribute
-        const currentVersionMatch = /Version: (\d+\.\d+)/g.exec(data);
-        if (!currentVersionMatch) {
-        console.error('Error finding current version in PHP file.');
-        return;
-        }
+    while (!isValid) {
+        const userInput = await ask();
 
-        const currentVersion = currentVersionMatch[1];
-        const newVersion = incrementVersion(currentVersion);
-        const updatedPhpContent = data.replace(currentVersionMatch[0], `Version: ${newVersion}`);
-
-        fs.writeFile(mainPluginFile, updatedPhpContent, 'utf8', (err) => {
-        if (err) {
-            console.error('Error writing the updated PHP file:', err);
+        if (/^\d+\.\d+\.\d+$/.test(userInput) && compareVersions(userInput, currentVersion) > 0) {
+            newVersion = userInput;
+            isValid = true;
         } else {
-            console.log(`Version updated to ${newVersion} in PHP file`);
+            console.error('Error: The version must be higher than the current version.');
         }
-        });
-    });
+    }
 }
 
-// Update the package.json file
-function updatePackageJsonFile() {
-    fs.readFile(packageJsonFile, 'utf8', (err, data) => {
-        if (err) {
-        console.error('Error reading the package.json file:', err);
-        return;
+// Function to update the JSON file
+async function updateInfoJsonFile() {
+    try {
+        const data = await fs.readFile(infoJsonFile, 'utf8');
+        const json = JSON.parse(data);
+        json.version = newVersion;
+        await fs.writeFile(infoJsonFile, JSON.stringify(json, null, 2), 'utf8');
+        console.log(`Version updated to ${newVersion} in JSON file`);
+    } catch (err) {
+        console.error('Error updating JSON file:', err);
+    }
+}
+
+// Function to update the PHP file
+async function updateMainPluginFile() {
+    try {
+        const data = await fs.readFile(mainPluginFile, 'utf8');
+        const currentVersionMatch = /Version: (\d+\.\d+\.\d+)/.exec(data);
+        if (!currentVersionMatch) {
+            console.error('Error finding current version in PHP file.');
+            return;
         }
 
-        try {
+        const updatedPhpContent = data.replace(currentVersionMatch[0], `Version: ${newVersion}`);
+        await fs.writeFile(mainPluginFile, updatedPhpContent, 'utf8');
+        console.log(`Version updated to ${newVersion} in PHP file`);
+    } catch (err) {
+        console.error('Error updating PHP file:', err);
+    }
+}
+
+// Function to update the package.json file
+async function updatePackageJsonFile() {
+    try {
+        const data = await fs.readFile(packageJsonFile, 'utf8');
         const packageJson = JSON.parse(data);
-        const currentVersion = packageJson.version;
-        const newVersion = incrementVersion(currentVersion);
         packageJson.version = newVersion;
-
-        fs.writeFile(packageJsonFile, JSON.stringify(packageJson, null, 2), 'utf8', (err) => {
-            if (err) {
-            console.error('Error writing the updated package.json file:', err);
-            } else {
-            console.log(`Version updated to ${newVersion} in package.json`);
-            }
-        });
-        } catch (parseError) {
-        console.error('Error parsing package.json:', parseError);
-        }
-    });
+        await fs.writeFile(packageJsonFile, JSON.stringify(packageJson, null, 2), 'utf8');
+        console.log(`Version updated to ${newVersion} in package.json`);
+    } catch (err) {
+        console.error('Error updating package.json file:', err);
+    }
 }
 
-updateInfoJsonFile();
-updateMainPluginFile();
-updatePackageJsonFile();
+// Close the readline interface
+function closeInterface() {
+    rl.close();
+}
+
+// Compare two semantic version numbers
+function compareVersions(versionA, versionB) {
+    const a = versionA.split('.').map(Number);
+    const b = versionB.split('.').map(Number);
+
+    for (let i = 0; i < a.length; i++) {
+        if (a[i] !== b[i]) {
+            return a[i] - b[i];
+        }
+    }
+
+    return 0;
+}
+
+// Call the update functions
+promptForNewVersion()
+    .then(updateInfoJsonFile)
+    .then(updateMainPluginFile)
+    .then(updatePackageJsonFile)
+    .then(closeInterface)
+    .catch((err) => {
+        console.error('Error updating files:', err);
+        closeInterface();
+    });
