35154672b8730c52b8d32c11254ebc6a30485052
Koen <koen@dolrondesign.nl>
2024-02-23 20:37:06 +0100

    Fixed the AWS s3 Credential error

--- offloader/MediaOffloaderInit.php
+++ offloader/MediaOffloaderInit.php
@@ -11,17 +11,42 @@ if (!defined('ABSPATH')) exit; // Exit if accessed directly
 require 'vendor/autoload.php';
 
 use Aws\S3\S3Client;
+use Aws\Credentials\Credentials;
+use GuzzleHttp\Promise\Promise;
 
-class s3MediaOffloaderInit {
+class s3MediaOffloaderInit
+{
     private $s3Client;
     private $offloader;
 
 
-    public function __construct(){
-       
+    public function __construct()
+    {
     }
-    
-    public function init(){
+
+    public function custom_wp_database_credentials_provider()
+    {
+        $provider = function () {
+            $promise = new Promise(function () use (&$promise) {
+                $options = get_option('wps3_image_offloader');
+                $aws_key = $options['wps3_aws_key'];
+                $aws_secret = $options['wps3_aws_secret'];
+
+                if (empty($aws_key) || empty($aws_secret)) {
+                    $promise->reject('No AWS credentials found in the database.');
+                } else {
+                    $promise->resolve(new Credentials($aws_key, $aws_secret));
+                }
+            });
+
+            return $promise;
+        };
+
+        return $provider;
+    }
+
+    public function init()
+    {
         $options = get_option('wps3_image_offloader');
         $bucket_name = $options['wps3_bucket_name'];
         $bucket_region = $options['wps3_bucket_region'];
@@ -34,14 +59,13 @@ class s3MediaOffloaderInit {
             return false;
         }
 
+        $customProvider = $this->custom_wp_database_credentials_provider();
+
         $this->s3Client = new S3Client([
             'region' => $bucket_region,
             'version' => 'latest',
             'use_aws_shared_config_files' => false,
-            'credentials' => [
-                'key' => $aws_key,
-                'secret' => $aws_secret
-            ],
+            'credentials' => $customProvider
         ]);
 
         $this->offloader = new s3MediaOffloader($this->s3Client, $bucket_name, $s3Path);
